18470ddefa6f1c4e402be0a1ac6b4bd860e948ee
Antonello Maddalena <80790317+Antonello-01@users.noreply.github.com>
2023-02-20 10:44:54 +0100

    20Feb2023, Antonello Maddalena e Gaetano Oliva, inizio sviluppo Account

--- iOS Foundation App/AccountSwiftUI.swift
+++ iOS Foundation App/AccountSwiftUI.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct AccountSwiftUI: View {
     var body: some View {
-        Text("Account")
+        VStack {
+            HStack {
+            Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                Spacer()
+                Text("Name Surname")
+                    .font(.title)
+                    .fontWeight(.bold)
+              
+            }.padding(50)
+            
+            VStack(spacing: -20){
+                HStack (){
+                    
+                    Text("Your Points of Interest")
+                        .fontWeight(.bold)
+                        .font(.title2)
+                    
+                    
+                    Spacer()
+                    Image(systemName: "plus")
+                        .foregroundColor(Color.blue)
+                    
+                }.padding()
+                
+                HStack{
+                    Image("ArcoTraiano")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(12)
+                }
+                .padding()
+            }
+            Spacer()
+        }
     }
 }
 
